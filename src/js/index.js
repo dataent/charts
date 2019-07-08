11535d906cdf9416544a3def6641cbdd18e2111d
dataent <38303879+dataent@users.noreply.github.com>
2019-07-07 22:11:24 -0500

    1.001

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import * as Charts from './chart';
+
+let dataent     = { };
+
+dataent.NAME    = 'Dataent Charts';
+dataent.VERSION = '1.2.0';
+
+dataent         = Object.assign({ }, dataent, Charts);
+
+export default dataent;
